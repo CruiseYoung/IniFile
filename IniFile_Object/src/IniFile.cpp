0b94851c85b599482f37d18517dd1494184a7de0
CruiseYoung <cruiseyoung@outlook.com>
2017-12-02 13:53:35 +0800

    修正SectionName、KeyName、Value合法性的判断

--- IniFile_Object/src/IniFile.cpp
+++ IniFile_Object/src/IniFile.cpp
@@ -128,7 +128,7 @@ static bool IsValidKeyValue(string const& s)
 
     for (unsigned int i = 0; i < s.size(); ++i)
     {
-        if (/*ispunct(s[i])*/s[i] > 0 && !isalnum(s[i]) && s[i] != '_')
+        if (/*ispunct(s[i])*/s[i] >= 0 && !isalnum(s[i]) && s[i] != '_')
         {
             return false;
         }
@@ -163,9 +163,6 @@ static bool IsRecord(string const& s)
         return false;
     }
 
-    return true;
-
-#if 0
     string s_key = s.substr(0, pos);
     TrimRight(s_key);
 
@@ -178,7 +175,6 @@ static bool IsRecord(string const& s)
     TrimLeft(s_value);
 
     return IsValidKeyValue(s_value);
-#endif
 }
 
 IniFile::IniFile(void)                                                      // Default constructor
